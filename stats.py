e92c2b2989a4d257137fa6e5f89c00f5f2571119
Finlay Maroney <finman292004@protonmail.com>
2024-02-24 15:41:54 -0500

    Added base stats.py

---
+++ stats.py
@@ -0,0 +1,45 @@
+import subprocess
+from datetime import datetime
+
+def rr(string: str) -> str:
+    while "  " in string:
+        string = string.replace("  ", " ")
+    return string
+
+
+host = subprocess.run(['hostname'], stdout=subprocess.PIPE).stdout.decode("utf-8").replace('\n','')
+
+storage = subprocess.run(['zfs', 'list'], stdout=subprocess.PIPE)
+first = True
+cur = datetime.now()
+timestamp = cur.strftime('%Y/%m/%d %H:%M:%S')
+
+out = open("/root/stats.csv", "a")
+
+for line in storage.stdout.decode("utf-8").split('\n'):
+    if first:
+        first = False
+        continue
+    if not line:
+        continue
+    # time,hostname,ZFS,Dataset,Used,Avail,Refer,Mountpoint
+    out.write(timestamp + "," + host +",ZFS," + ",".join(rr(line).split(" ")) + "\n")
+
+ram = subprocess.run(['free', '-h'], stdout=subprocess.PIPE)
+rdata = rr(ram.stdout.decode("utf-8").split('\n')[1]).split(" ")
+rdata[0] = "MEM"
+# time,hostname,MEM,total,used,free,shared,cache,available
+
+out.write(timestamp + "," + host + "," + ",".join(rdata) + "\n")
+
+cpu = subprocess.run(['/root/cpuusage.sh'], stdout=subprocess.PIPE).stdout.decode("utf-8").replace('\n','')
+# time,hostname,CPU,percentage
+out.write(timestamp + "," + host + ",CPU," + cpu + "%\n")
+
+status = subprocess.run(['systemctl', 'status'], stdout=subprocess.PIPE).stdout.decode("utf-8").split('\n')[1]
+
+# time,hostname,SERV,status (running,degraded,etc)
+out.write(timestamp + "," + host + ",SERV," + status.split(":")[1][1:] + "\n")
+
+out.close()
+
