f7656a8bae7e1a74b7a146e6c970bba13e43982b
Finlay Maroney <finman292004@protonmail.com>
2024-02-24 16:03:24 -0500

    fixed pull

--- pull.sh
+++ pull.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-destdir=/var/backups/stats/
+destdir=/var/backups/stats
 hosts=("10.7.0.7" "10.7.0.6" "10.7.0.5" "10.8.0.1" "10.8.0.97")
 key=/var/backups/.ssh/backup
 
 for host in "${hosts[@]}"
 do
-    rsync -e "ssh -i $key" -avP --remove-source-files --ignore-existing "backup@$host:stats.csv" $destdir
+    rsync -e "ssh -i $key" -avP --remove-source-files --ignore-existing "backup@$host:stats.csv" $destdir/$host-stats.csv
 done
 
